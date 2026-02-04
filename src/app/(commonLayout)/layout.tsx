a7512497e66b0875f0e37b653dcd2d7adf165078
Imtiaz Samir <imtiazsamir91@gmail.com>
2026-02-04 12:59:22 +0600

    hero

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,9 +1,11 @@
+import { Hero47 } from "@/components/layout/hero47";
 import { Navbar1 } from "@/components/layout/Navbar";
 
 export default function Commonlayout({children}:{children:React.ReactNode}) {
   return (
     <div>
          <Navbar1 />
+         <Hero47/>
       {children}
     </div>
   );
